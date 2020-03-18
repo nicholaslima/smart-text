82cbd4cf434a5acd528d031cb4037b4aed845068
nicholas caetano <nichollas36@hotmail.com>
2020-03-18 02:17:57 -0300

    correcao erros usuario

--- App/JS/Controllers/ControleController.js
+++ App/JS/Controllers/ControleController.js
@@ -3,26 +3,115 @@ class ControleController {
         this._acertos = 0;
         this._erros = 0;
         this._tempo = 10;
+        this._textoAluno;
+        this._textoTeste;
+        this._tamanhoDigitado = 0;
+        this._letraAtual;
+        this._posicoesErros = [];
+        this._inputsTeste;
+        this._inputsAluno;
+
+        this._acertosElemento;
+        this._errosElemento;
+        this._tempoElemento;
     }
-    comparartexto() {
-        var qtd = this._texto.length;
-        if (this._texto[qtd - 1] === this._texto2[qtd - 1]) {
-            this._acertos++;
-        }
-        else {
-            this._erros++;
-        }
-    }
+
     getAcertos() {
         return this._acertos;
     }
     getErros() {
         return this._erros;
     }
-    setTexto(texto) {
-        this._texto = texto;
+    setTextoAluno(textoAluno) {
+        this._textoAluno = textoAluno;
     }
-    setTexto2(texto2) {
-        this._texto2 = texto2;
+    setTextoTeste(textoTeste) {
+        this._textoTeste = textoTeste;
+    }
+
+    setAcertosElemento(acertosElemento){
+        this._acertosElemento = acertosElemento;
+    }
+
+    setErrosElemento(errosElemento){
+        this._errosElemento = errosElemento;
+    }
+
+    setTempoElemento(tempoElemento){
+        this._tempoElemento = tempoElemento;
+    }
+
+    setLetraAtual(letra){
+        this._letraAtual = letra;
+    }
+
+    comparartexto(valido) {
+        if(valido !== false){
+            if(this._textoAluno[this._tamanhoDigitado - 1] === this._textoTeste[this._tamanhoDigitado - 1]) {
+                this._acertos++;
+            }
+            else {
+                this._erros++;
+                this.capturarErros(this._tamanhoDigitado -1);
+                console.log(this._posicoesErros);
+            }  
+            this.setPontuacao();
+        }
+    }
+
+    dividirTextoTeste(){
+        this._inputsTeste = this._textoTeste.split();
+    }
+
+    dividirTextoAluno(){
+        this._inputsAluno = this._textoAluno.split();
+    }
+
+    capturarErros(posicao){
+        this._posicoesErros.push(posicao);
+    }
+
+    setTamanho(tamanho){
+        if(tamanho !== false){
+            this._tamanhoDigitado = tamanho;
+            return this._tamanhoDigitado;
+        }else{
+            return false;
+        }
+    }
+
+    filtroCorrecaoUsuario(textoQtd){
+        if(textoQtd < this._tamanhoDigitado){
+            if(this.encontrarErros(textoQtd)){
+                console.log(this._posicoesErros.splice(textoQtd));
+                this._erros--;
+                this.setPontuacao();
+            }else{
+                this._acertos--;
+                this.setPontuacao();
+            }
+            console.log(this._tamanhoDigitado);
+            console.log(textoQtd);
+            console.log(this.encontrarErros(textoQtd));
+            console.log(this._posicoesErros);
+            console.log(this._posicoesErros);
+            console.log(this._posicoesErros);
+            this.dividirTextoTeste();
+            this.dividirTextoAluno();
+            console.log(this._inputsTeste);
+            console.log(this._inputsAluno);
+            return false;
+        }else{
+            return textoQtd;
+        }
+    }
+
+    encontrarErros(textoQtd){
+        return this._posicoesErros.includes(textoQtd);
+    }
+
+    setPontuacao(){
+        this._acertosElemento.text(this._acertos);
+        this._errosElemento.text(this._erros);
     }
 }
--- App/JS/app.js
+++ App/JS/app.js
@@ -1,23 +1,37 @@
-const texto = $("#texto").val();
-const texto2 = $("#texto2").text();
+
+const textoTeste = $("#textoTeste").text();
 const acertos = $("#acertos").text();
 const erros = $("#erros").text();
 const tempo = $("#tempo").text();
+
 const acertosElemento = $("#acertos");
 const errosElemento = $("#erros");
 const tempoElemento = $("#tempo");
+
 var controle = new ControleController();
+
 document.getElementById("enviar").onclick = function (event) {
     event.preventDefault();
     let jogador = new Jogador(texto, acertos, erros, tempo);
     console.log(jogador);
 };
-document.getElementById("texto").oninput = () => {
-    const texto = $("#texto").val();
-    const texto2 = $("#texto2").text();
-    controle.setTexto(texto);
-    controle.setTexto2(texto2);
-    controle.comparartexto();
-    acertosElemento.text(controle.getAcertos());
-    errosElemento.text(controle.getErros());
+
+document.getElementById("textoAluno").oninput = (input) => {
+    const letra = input.data;
+    const textoAluno = $("#textoAluno").val();
+
+    controle.setTextoAluno(textoAluno);
+    controle.setTextoTeste(textoTeste);
+    controle.setAcertosElemento(acertosElemento);
+    controle.setErrosElemento(errosElemento);
+    controle.setTempoElemento(tempoElemento);
+    controle.setLetraAtual(letra);
+    const textoQtd = textoAluno.length;
+      
+       controle.comparartexto(
+                    controle.setTamanho(
+                        controle.filtroCorrecaoUsuario(textoQtd))
+                    );
+                 
 };
+
