71486f38d73e80a4d7c4740dc7d86afb06d093df
nicholas caetano <nichollas36@hotmail.com>
2020-09-13 03:34:13 -0300

    authentication and register ready

--- front/src/pages/signin/index.tsx
+++ front/src/pages/signin/index.tsx
@@ -1,12 +1,48 @@
 
 
 
-import React from 'react';
+import React,{ useState } from 'react';
 import { FiArrowRight } from 'react-icons/fi';
 import { Form,Titulo,Input,Container} from './style';
 import Header from '../../components/header/header';
+import api from '../../config/api';
 
-const hanking:React.FC = () => {
+interface Data{
+    token: string;
+}
+
+interface authResp{
+    data: Data;
+}
+
+const Hanking:React.FC = () => {
+
+    const [ email,setEmail ] =  useState('');
+    const [ password,setPassword ] =  useState('');
+
+    const userdata = {
+        email,
+        senha: password
+    }
+
+    async function handdleAuth(){
+
+            const resp: authResp = await api.post('user/auth',userdata);
+            
+            const token = resp.data.token;
+            saveToken(token);
+            limparForm();   
+    }
+
+    function limparForm(){
+        setEmail('');
+        setPassword('');
+    }
+
+    function saveToken(token: string){
+        localStorage.setItem('jwt',token);
+    }
+
     return(
         <>
             <Header></Header>
@@ -14,9 +50,9 @@ const hanking:React.FC = () => {
                 
                 <Form>
                         <Titulo>Entre em sua conta</Titulo>
-                        <Input type="email" placeholder="insira seu email" />
-                        <Input type="password" placeholder="insira sua senha" />
-                        <button type="button" className="btnRoxo"><FiArrowRight></FiArrowRight> login</button>
+                        <Input type="email" placeholder="insira seu email" value={ email } onChange={ (e) => setEmail(e.target.value)} />
+                        <Input type="password" placeholder="insira sua senha"  value={ password } onChange={ (e) => setPassword(e.target.value)}/>
+                        <button type="button" className="btnRoxo" onClick={ handdleAuth }><FiArrowRight></FiArrowRight> login</button>
                 </Form>
             </Container>
         </>
@@ -25,4 +61,4 @@ const hanking:React.FC = () => {
 
 }
 
-export default hanking;
+export default Hanking;
--- front/src/pages/signup/index.tsx
+++ front/src/pages/signup/index.tsx
@@ -1,11 +1,41 @@
 
-import React from 'react';
+import React,{ useState } from 'react';
 import { Input,Form,Titulo, Description,Container,BlockDescription,BtnVoltar } from './style';
 import Header from '../../components/header/header';
 import { FiArrowLeft,FiBookOpen } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import api from '../../config/api';
+
 
 const Signup: React.FC = () => {
+
+    const [ name,setName ] = useState('');
+    const [ email,setEmail ] = useState('');
+    const [ password,setPassword ] = useState('');
+    const [ sexo,setSexo ] = useState('');
+    const [ age,setAge] = useState('');
+
+    async function handdleRegister(){
+        const data = {
+            nome: name,
+            email: email,
+            senha: password,
+            sexo: sexo,
+            idade: age
+        }
+
+       const resp = await api.post('/user/create',data);
+       apagarForm();
+    }
+
+    function apagarForm(){
+        setName('');
+        setEmail('');
+        setPassword('');
+        setSexo('');
+        setAge('');
+    }
+    
     return(
         <>
         <Header></Header>
@@ -17,10 +47,12 @@ const Signup: React.FC = () => {
                 </BlockDescription>
                 <Form>
                     <Titulo>Cadastre seu Acesso:</Titulo>
-                    <Input type="text" placeholder="insira seu nome" />
-                    <Input type="email" placeholder="insira seu email" />
-                    <Input type="password" placeholder="insira sua senha" />
-                    <button type="button" className="btnRoxo"><FiBookOpen></FiBookOpen> cadastrar</button>
+                    <Input type="text" placeholder="insira seu nome" value={ name } onChange={ e => setName(e.target.value) }/>
+                    <Input type="email" placeholder="insira seu email" value={ email } onChange={ e => setEmail(e.target.value) }/>
+                    <Input type="password" placeholder="insira sua senha" value={ password } onChange={ e => setPassword(e.target.value) }/>
+                    <Input type="text" placeholder="insira sua idade" value={ age } onChange={ e => setAge(e.target.value) }/>
+                    <Input type="text" placeholder="insira seu sexo" value={ sexo } onChange={ e => setSexo(e.target.value) }/>
+                    <button type="button" className="btnRoxo" onClick={ handdleRegister }><FiBookOpen></FiBookOpen> cadastrar</button>
                 </Form>
             </Container>
         </>
